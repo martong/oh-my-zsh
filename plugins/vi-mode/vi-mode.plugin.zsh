68d3fe1d3adef893b1c0cdde48681801c305c131
Marton Gabor <gabor.marton@ericsson.com>
2014-03-11 17:23:46 +0100

    Modified vi-mode plugin, RPS1 untouched.

--- plugins/vi-mode/vi-mode.plugin.zsh
+++ plugins/vi-mode/vi-mode.plugin.zsh
@@ -14,6 +14,8 @@ function zle-keymap-select zle-line-init zle-line-finish {
   zle -R
 }
 
+#function zle-line-init() { zle -K vicmd; }
+
 zle -N zle-line-init
 zle -N zle-line-finish
 zle -N zle-keymap-select
@@ -26,10 +28,11 @@ if [[ "$MODE_INDICATOR" == "" ]]; then
 fi
 
 function vi_mode_prompt_info() {
+  #echo "km:$KEYMAP"
   echo "${${KEYMAP/vicmd/$MODE_INDICATOR}/(main|viins)/}"
 }
 
 # define right prompt, if it wasn't defined by a theme
-if [[ "$RPS1" == "" && "$RPROMPT" == "" ]]; then
-  RPS1='$(vi_mode_prompt_info)'
-fi
+#if [[ "$RPS1" == "" && "$RPROMPT" == "" ]]; then
+  #RPS1='$(vi_mode_prompt_info)'
+#fi
