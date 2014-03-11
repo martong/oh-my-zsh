877df33b5270463529cdc61346f9008e0d114893
Marton Gabor <gabor.marton@ericsson.com>
2014-03-11 17:38:31 +0100

    Modified history.zsh to avoid duplicated history in case of substring history search.

--- lib/history.zsh
+++ lib/history.zsh
@@ -9,6 +9,8 @@ setopt append_history
 setopt extended_history
 setopt hist_expire_dups_first
 setopt hist_ignore_dups # ignore duplication command history list
+# ignore all dups to make hist substring search work nicely.
+setopt hist_ignore_all_dups
 setopt hist_ignore_space
 setopt hist_verify
 setopt inc_append_history
